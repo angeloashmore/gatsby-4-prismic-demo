55ef781cce653575722e44c3856d43a85964a020
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-10-06 08:37:23 -1000

    fix: custom types api usage

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,13 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet-async",
     "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
     {
       resolve: "gatsby-source-prismic",
       options: {
         repositoryName: process.env.GATSBY_PRISMIC_REPOSITORY_NAME,
         accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        // customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-        schemas: {
-          page: require("./custom_types/page.json"),
-        },
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
         releaseID: process.env.PRISMIC_RELEASE_ID,
         linkResolver: require("./src/linkResolver").linkResolver,
       },
